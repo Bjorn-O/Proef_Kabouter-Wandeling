4e30bc3a7fcefdc740decaec83636eb2e035c270
Jorikos <26204@ma-web.nl>
2023-03-09 15:45:27 +0100

    Custom event support added

--- Assets/Scripts/Notification.cs
+++ Assets/Scripts/Notification.cs
@@ -3,6 +3,12 @@ using System.Collections.Generic;
 using Unity.Notifications.Android;
 using UnityEngine.Assertions;
 using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class NotificationEvent : UnityEvent<string, string, int>
+{
+}
 
 public class Notification : MonoBehaviour
 {
@@ -21,19 +27,13 @@ public class Notification : MonoBehaviour
         AndroidNotificationCenter.RegisterNotificationChannel(c);
     }
 
-
-    private void Update()
-    {
-        SendNotification("Test", "Test 2", 0);
-    }
-
     /// <summary>
     /// Function used to send notifications
     /// </summary>
     /// <param name="notifTitle">Title of the notification.</param>
     /// <param name="notifText">Text of the notification.</param>
     /// <param name="fireTime">Amount of time to delay the notification with in seconds.</param>
-    private void SendNotification(string notifTitle, string notifText, int fireTime)
+    public void SendNotification(string notifTitle, string notifText, int fireTime)
     {
         notification.Title = notifTitle;
         notification.Text = notifText;
