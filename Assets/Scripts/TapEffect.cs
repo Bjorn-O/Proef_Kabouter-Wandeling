00bd95ff99eb9a77c5dc269f5dfc7582dddb8b42
Jorikos <26204@ma-web.nl>
2023-03-13 13:17:03 +0100

    Create scene + script

---
+++ Assets/Scripts/TapEffect.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TapEffect : MonoBehaviour
+{
+    [SerializeField] private Vector3 _vector;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
